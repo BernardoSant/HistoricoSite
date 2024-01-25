2f5fa020df85695dad8ec8b431521d2459efa952
Bernardo Santos Magalhaes <bernardosantosmagalhaesm@gmail.com>
2024-01-25 11:09:36 -0300

    Atualização - api

--- APHISTORICO/src/components/TabelaEmpresa.jsx
+++ APHISTORICO/src/components/TabelaEmpresa.jsx
@@ -12,16 +12,9 @@ gap:0.5em;
 overflow: auto;
 `;
 
-const Input = styled.input`
-width: 100%;
-border-radius: 5px;
-padding: 0.3em;
-padding-left: 0.8em;
-border: 1px solid ;
-border-color: #848484;
-`;
 const H1 = styled.h1`
 font-weight: 600;
+margin-top: 5px;
 `;
 
 export const TabelaAdicionarEmpresa = () => {
@@ -30,64 +23,57 @@ export const TabelaAdicionarEmpresa = () => {
             <Header>
                 <h1 className='w-full flex justify-center items-center text-3xl'>Adcionar Empresa</h1>
                 <nav className=' flex flex-col overflow-auto '>
-                    <div>
-                        <H1>Nome*</H1>
-                        <Input type="text" />
-                    </div>
-                    <div>
-                        <H1>Email</H1>
-                        <Input type="email" />
-                    </div>
-                    <div>
-                        <H1>CEP*</H1>
-                        <InputMask mask={"99999-999"} className="border-[1px] border-[#848484] rounded-md px-3 py-[0.3em]" type="" />
-                    </div>
-                    <div>
-                        <H1>Lougradouro*</H1>
-                        <Input type="text" />
-                    </div>
-                    <div>
-                        <H1>Bairro*</H1>
-                        <Input type="text" />
-                    </div>
-                    <div>
-                        <H1>Número*</H1>
-                        <Input type="number" />
-                    </div>
-                    <div>
-                        <H1>Complemento</H1>
-                        <Input type="text" />
-                    </div>
-                    <div>
-                        <H1>Cidade</H1>
-                        <Input type="text" />
-                    </div>
-                    <div>
-                        <H1>CNPJ*</H1>
-                        <InputMask mask="99.999.999/9999-99" className="border-[1px] border-[#848484] rounded-md px-3 py-[0.3em]" type="text" />
-                    </div>
-                    <div>
-                        <H1>Responsável</H1>
-                        <Input type="text" />
-                    </div>
 
-                    <div className='flex justify-between items-end'>
-                        <dir className="flex flex-row gap-10">
-                            <nav>
-                                <H1 className='text-xl'>Campos Adcionais</H1>
-                                <H1>Data de Contrato</H1>
-                                <Input type="date" />
-                            </nav>
-                            <nav className='flex flex-col justify-end'>
-                                <H1>Situação*</H1>
-                                <select id="SituaçãoEmpre" name="SituaçãoEmpre" className="border-[1px] border-[#848484] rounded-md px-3 py-[0.3em]">
-                                    <option value="Particular">Particular</option>
-                                    <option value="Contrato">Contrato</option>
-                                </select>
-                            </nav>
+                    <H1>Nome*</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Email</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>CEP*</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Lougradouro*</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Bairro*</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Número*</H1>
+                    <input type="number" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Complemento</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Cidade</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>CNPJ*</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+                    <H1>Responsável</H1>
+                    <input type="text" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2 " />
+
+
+                    <table className='flex justify-between items-start md:items-end w-full mt-3 flex-col  md:flex-row gap-6 max-w-[40em] ' >
+                        <dir className="flex flex-col ">
+                            <H1 className='text-xl'>Campos Adcionais</H1>
+                            <div className='flex gap-6 flex-wrap'>
+                                <nav>
+                                    <H1>Data de Contrato</H1>
+                                    <input type="date" className="border-2 max-w-[40em] w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2" />
+                                </nav>
+                                <nav className='flex flex-col justify-end'>
+                                    <H1>Situação*</H1>
+                                    <select id="SituaçãoEmpre" name="SituaçãoEmpre" className="border-[1px] border-[#848484] rounded-md px-3 py-[0.3em]">
+                                        <option value="Particular">Particular</option>
+                                        <option value="Contrato">Contrato</option>
+                                    </select>
+                                </nav>
+                            </div>
                         </dir>
-                        <dir><button className='bg-orange-400 py-2 px-7 rounded-lg border-2 border-orange-500 font-semibold hover:text-white hover:scale-95 duration-500'>Salvar</button></dir>
-                    </div>
+                        <button type='submit' className='w-full md:w-auto bg-orange-400 py-2 px-7 rounded-lg border-2 border-orange-500 font-semibold hover:text-white hover:scale-95 duration-500'>Salvar</button>
+                    </table>
                 </nav>
             </Header>
         </>
---
+++ API/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3030, () =>{
+    console.log(" Servidor iniciado na porta 3030: http://localhost:3030")
+});
