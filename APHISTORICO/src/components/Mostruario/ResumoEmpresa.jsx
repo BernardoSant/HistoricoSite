593beb94136c7ebd74f7ffdf306a4862eee609f3
BernardoSant <bernardosantosmagalhaesm@gmail.com>
2024-02-16 16:44:55 -0300

    Resumo empresa 1.0

--- APHISTORICO/src/components/Mostruario/ResumoEmpresa.jsx
+++ APHISTORICO/src/components/Mostruario/ResumoEmpresa.jsx
@@ -1,61 +1,183 @@
-import styled from 'styled-components';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { useGlobalContext } from '../../global/Global';
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useGlobalContext } from "../../global/Global";
 
-const Article = styled.article`
+const Footer = styled.footer`
   height: 100%;
   width: 100%;
-  padding-left: 1em;
-  padding-right: 1em;
-  padding-bottom: 1em;
-  padding-top: 1em;
+  padding: 1em;
   display: flex;
   flex-wrap: wrap;
   max-width: 50em;
   justify-content: start;
   align-content: start;
-  flex-direction: row;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  margin-bottom: 7px;
+  border-radius: 1em;
+  background: #f97316;
+  box-shadow: inset 5px -5px 10px #9f4a0e, inset -5px 5px 10px #ff9c1e;
+`;
+
+const Section = styled.section`
+  width: 100%;
+  display: flex;
+  gap: 7px;
+  flex-wrap: wrap;
+  flex: 1 1 0%;
+`;
+
+const Article = styled.article`
+  flex: 1 1 0%;
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+  min-height: 5em;
+`;
+
+const Dir = styled.div`
+  border-top-right-radius: 1em;
+  border-top-left-radius: 1em;
+  border-bottom-right-radius: 0.3em;
+  border-bottom-left-radius: 0.3em;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0%;
+  align-content: center;
+  padding: 0.7em;
+  padding-left: 1em;
+  padding-right: 1em;
+  background: #f97316;
+  box-shadow: inset 3px -3px 10px #9f4a0e, inset -5px 5px 10px #ff9c1e;
+  flex: none;
+  z-index: 10;
 `;
 
 const Div = styled.div`
-border-top-right-radius: 1em;
-border-top-left-radius: 1em;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-align-content: center;
-padding: 0.7em;
-padding-left: 1em;
-padding-right: 1em;
-background: #ea580c;
-box-shadow: inset 5px -0px 10px #a15d26,
-            inset -5px 7px 10px #ffc752;
-font-weight: 600;
-
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  padding-left: 1em;
+  padding-right: 1em;
+  background-color: #d8d6d679;
+  margin-top: -5px;
+  z-index: 0;
+  padding-top: 10px;
+  overflow: auto;
+  border-bottom-right-radius: 1em;
+  border-bottom-left-radius: 1em;
 `;
+
+const H1 = styled.h1`
+  font-weight: 700;
+  font-size: larger;
+`;
+
+const H2 = styled.h2`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-weight: 600;
+  font-size: medium;
+`;
+
+const H3 = styled.h3`
+  display: flex;
+  flex-direction: row;
+  font-weight: 600;
+`;
+
+const P = styled.p`
+  text-align: center;
+  width: 100%;
+`;
+
 export const ResumoEmpresa = () => {
+  const { empresa } = useGlobalContext();
 
-    return(
+  return (
+    <Footer>
+      <Header>
+        <thead className="flex justify-center items-center py-4 gap-5">
+          <th className="text-start text-3xl pt-1">Resumo Mensal</th>
+          <select name="" id="">
+            <option value="01">Janeiro</option>
+          </select>
+        </thead>
+      </Header>
+
+      <Section>
         <Article>
+          <Dir>
+            <H1> Nota Fiscal Recebida</H1>
 
-            <Div>
+            <H2>
+              <P>Nome</P>
+              <P>CNPJ</P>
+            </H2>
+          </Dir>
+          <Div>
+            <H3></H3>
+          </Div>
+          <Dir>
+            <H1> Nota Fiscal Recebida</H1>
 
-            </Div>
-
-            <Div>
-                
-            </Div>
-            
-            <Div>
-
-            </Div>
-
-            <Div>
-
-            </Div>
-        ola
+            <H2>
+              <P>Nome</P>
+              <P>CNPJ</P>
+            </H2>
+          </Dir>
+          <Div>
+            <H3></H3>
+          </Div>
         </Article>
-    )
-}
+
+        <Article>
+          <Dir>
+            <H1>Empresas Cadastradas</H1>
+
+            <H2>
+              <P>Nome</P>
+              <P>CNPJ</P>
+            </H2>
+          </Dir>
+          <Div>
+            <H3></H3>
+          </Div>
+        </Article>
+      </Section>
+
+      <Section>
+        <Article>
+          <Dir>
+            <H1>Empresas Cadastradas</H1>
+
+            <H2>
+              <P>Nome</P>
+              <P>CNPJ</P>
+            </H2>
+          </Dir>
+          <Div>
+            {empresa.map((empresas) => (
+              <H3>
+                <P>{empresas.siglaEmpresa}</P>
+                <P>{empresas.cnpjEmpresa}</P>
+              </H3>
+            ))}
+          </Div>
+        </Article>
+
+        <Article>
+          <Dir>Ganhos Mensal</Dir>
+          <Div>OPA</Div>
+        </Article>
+      </Section>
+    </Footer>
+  );
+};
